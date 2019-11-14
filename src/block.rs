0f51a219e19bc177952a70bf37357369c6d99c71
Collin Jones <cjones28@luc.edu>
2019-11-13 23:45:39 -0600

    starting point for project

---
+++ src/block.rs
@@ -0,0 +1,24 @@
+use Super::*; //imports lib.rs
+
+pub struct Block{
+    pub index: u32, //where in the blockchain the block is at
+    pub timeStamp: u128 , //time block is created
+    pub hash: BlockHash, //hash for the block
+    pub lastBlockHash: BlockHash, //hash of the previous block in blockchain
+    pub nonce: u32, // this is for the proof of work when mining or doing transactions
+
+}
+
+impl Block{
+    pub fn new(index: u32, timeStamp: u128, lastBlockHash: BlockHash, nonce: u32) -> Self{
+        Block{
+            index,
+            timeStamp,
+            hash: vec![0;32], // makes vector array of 32 zeros. will be used for SHA-256 hashing
+            lastBlockHash,
+            nonce
+        }
+    }
+
+
+}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+type BlockHash = Vec<u8>;
