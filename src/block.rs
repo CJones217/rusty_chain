da265d3f70d5f2141894f19adc52e7e473f146d4
Collin Jones <cjones28@luc.edu>
2019-11-18 22:15:54 -0600

    hash is not working not sure why

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,8 @@
-use crate::BlockHash;
+use crate::*;
 use crypto_hash::*;
 use std::fmt;
 use hex::*;
+use crypto_hash::Algorithm::SHA256;
 
 pub struct Block{
     pub index: u32, //where in the blockchain the block is at
@@ -9,31 +10,39 @@ pub struct Block{
     pub hash: BlockHash, //hash for the block
     pub lastBlockHash: BlockHash, //hash of the previous block in blockchain
     pub nonce: u32, // this is for the proof of work when mining or doing transactions
+    pub payload: String,
 
 }
 impl fmt::Debug for Block{ //needs to be before impl Block. not sure why TODO look into this
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-    write!(f,"Block ~ Index: {} ~ time created: {} ~ hash: {} ~ last blocks hash: {} ~ nonce (POW): {}",
-           &self.index, &self.timeStamp, &hex::encode(&self.hash), &hex::encode(&self.lastBlockHash), &self.nonce)
+    write!(f,"Block ~ Index: {} ~ time created: {} ~ hash: {} ~ last blocks hash: {} ~ nonce (POW): {} ~ payload: {}",
+           &self.index, &self.timeStamp, &hex::encode(&self.hash), &hex::encode(&self.lastBlockHash), &self.nonce, &self.payload)
     }
 
 }
 
 impl Block{
-    pub fn new(index: u32, timeStamp: u128, lastBlockHash: BlockHash, nonce: u32) -> Self{
+    pub fn new(index: u32, timeStamp: u128, lastBlockHash: BlockHash, nonce: u32, payload: String) -> Self{
         Block{
             index,
             timeStamp,
             hash: vec![0;32], // makes vector array of 32 zeros. will be used for SHA-256 hashing
             lastBlockHash,
-            nonce
+            nonce,
+            payload,
         }
     }
     pub fn bytes(&self) -> Vec<u8>{
-
+        let mut b = vec![];
+        b.extend(&u32_bits_to_4bytes(&self.index));
+        b.extend(&u128_bits_to_16bytes(&self.timeStamp));
+        b.extend(&self.lastBlockHash);
+        b.extend(&u32_bits_to_4bytes(&self.nonce));
+        b.extend(self.payload.as_bytes());
+        b
     }
     pub fn hash(&self) -> Vec<u8> {
-        Vec::from(hex_digest(Algorithm::SHA256, &self.bytes()))
+        digest(SHA256,&self.bytes())
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use rustychainlib::*;
 
 fn main() {
     println!("Hello, world!");
-    let block = Block::new(0,0,vec![0;32],0);
-
+    let block = Block::new(2,4,vec![0;32],6, "test block".to_owned());
     println!("{:?}",&block);
+    let block_hash = block.hash;
+    println!("{:?}", &block_hash);
 }
+//
